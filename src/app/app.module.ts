2c6179245df23714fc14d0954d589d4f01606550
Hemrajsinh Varachhia <hemrajkv793@gmail.com>
2021-08-16 23:28:27 -0700

    first commit with component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import {ServerComponent} from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { warningalert } from './warningalert/warningalert.component';
+import { successalert } from './successalert/successalert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    ServerComponent,
+    AppComponent,
+    ServersComponent,
+    warningalert,
+    successalert
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
